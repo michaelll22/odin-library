a0ae613b49f79180320d28b196905b294248eb96
michael <91875751+michaelll22@users.noreply.github.com>
2025-09-14 14:40:49 +0800

    Enhance book form with required fields and status selection; update book list reference; add remove book function

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 const bookForm = document.querySelector("#book-form");
-const bookList = document.querySelector(".book-list");
+const bookList = document.querySelector(".books");
 const addBookBtn = document.querySelector(".add");
 const closeButton = document.querySelector(`.close-btn`);
 const dialog = document.querySelector("dialog");
 
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+let myLibrary = [];
 
-  const titleInput = document.querySelector("#title");
-  const authorInput = document.querySelector("#author");
-  const pagesInput = document.querySelector("#pages");
-
-  let book = new Book(titleInput.value, authorInput.value, pagesInput.value);
-  addBookToLibrary(book);
-  console.table(myLibrary);
-  dialog.close();
-});
-
-const myLibrary = [];
+function removeBook(bookElement, bookObj) {
+  bookList.removeChild(bookElement);
+  myLibrary = myLibrary.filter((b) => b !== bookObj);
+}
 
 function Book(title, author, pages) {
-  // the constructor...
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
 
 function addBookToLibrary(book) {
-  // take params, create a book then store it in the array
   myLibrary.push(book);
-  const addBook = document.createElement("p");
-  addBook.textContent = `${book.title} by ${book.author}`;
-  bookList.appendChild(addBook);
+
+  const bookInfo = document.createElement("li");
+  const bookRemove = document.createElement("button");
+
+  bookInfo.textContent = `${book.title} by ${book.author}`;
+  bookRemove.textContent = "Remove";
+
+  bookList.appendChild(bookInfo);
+  bookInfo.appendChild(bookRemove);
+
+  bookRemove.addEventListener("click", () => {
+    removeBook(bookInfo, book);
+  });
 }
+
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const titleInput = document.querySelector("#title").value;
+  const authorInput = document.querySelector("#author").value;
+  const pagesInput = document.querySelector("#pages").value;
+
+  let book = new Book(titleInput, authorInput, pagesInput);
+  addBookToLibrary(book);
+  console.table(myLibrary);
+  dialog.close();
+  bookForm.reset();
+});
 
 addBookBtn.addEventListener("click", () => {
   dialog.showModal();
@@ -40,4 +54,5 @@ addBookBtn.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   dialog.close();
+  bookForm.reset();
 });
