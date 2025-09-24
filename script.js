b1e7e8d324bf66e2daaf7810d9b4d513314d19a8
michael <91875751+michaelll22@users.noreply.github.com>
2025-09-24 18:39:48 +0800

    Refactor Book class to use static methods for managing book list; implement handleBook function for form submission

--- script.js
+++ script.js
@@ -4,66 +4,118 @@ const addBookBtn = document.querySelector(".add");
 const closeButton = document.querySelector(`.close-btn`);
 const dialog = document.querySelector("dialog");
 
-let myLibrary = [];
+// let myLibrary = [];
 
-function Book(title, author, pages, status) {
-  this.uid = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
+// function Book(title, author, pages, status) {
+//   this.uid = crypto.randomUUID();
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.status = status;
+// }
+
+class Book {
+  constructor(title, author, pages, status) {
+    this.uid = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
+
+  static bookList = [];
+
+  static bookCount = 0;
+
+  static viewBooks() {
+    console.log(this.bookCount);
+    console.log(this.bookList);
+  }
+
+  static addBook(book) {
+    this.bookCount++;
+    this.bookList.push(book);
+    console.log(this.bookList);
+  }
+
+  static removeBook(index) {
+    Book.bookList.splice(index, 1);
+  }
+
+  update() {
+    
+  }
 }
 
-function addBookToLibrary() {
+const book1 = new Book("book1", "author1", 82, "Not Reading");
+const book2 = new Book("book2", "author2", 102, "Reading");
+
+Book.addBook(book1);
+Book.addBook(book2);
+
+// function addBookToLibrary() {
+//   const title = document.querySelector("#title").value;
+//   const author = document.querySelector("#author").value;
+//   const pages = document.querySelector("#pages").value;
+//   const status = document.querySelector("#status").value;
+
+//   let newBook = new Book(title, author, pages, status);
+
+//   myLibrary.push(newBook);
+//   addBookCard(newBook);
+// }
+
+// function addBookCard(book) {
+//   const bookCard = document.createElement("div");
+//   bookCard.classList.add("bookCard");
+
+//   const cardTitle = document.createElement("h4");
+//   const cardAuthor = document.createElement("p");
+//   const cardPages = document.createElement("p");
+//   const cardStatus = document.createElement("p");
+
+//   const cardRemove = document.createElement("button");
+//   cardRemove.textContent = "Remove";
+
+//   cardTitle.textContent = book.title ? `Title: ${book.title}` : "Unknown Title";
+//   cardAuthor.textContent = `Author: ${book.author}`;
+//   cardPages.textContent = book.pages
+//     ? `Pages: ${book.pages}`
+//     : "Page count missing";
+//   cardStatus.textContent = `Status: ${book.status}`;
+
+//   bookCard.appendChild(cardTitle);
+//   bookCard.appendChild(cardAuthor);
+//   bookCard.appendChild(cardPages);
+//   bookCard.appendChild(cardStatus);
+//   bookCard.appendChild(cardRemove);
+
+//   bookList.appendChild(bookCard);
+
+//   cardRemove.addEventListener("click", () => removeBook(bookCard, book.uid));
+// }
+
+// function removeBook(bookElement, bookId) {
+//   bookElement.remove();
+//   myLibrary = myLibrary.filter((book) => book.uid !== bookId);
+// }
+
+function handleBook() {
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
   const status = document.querySelector("#status").value;
 
-  let newBook = new Book(title, author, pages, status);
+  let newBook = new Book(title, author, parseInt(pages), status);
 
-  myLibrary.push(newBook);
-  addBookCard(newBook);
-}
-
-function addBookCard(book) {
-  const bookCard = document.createElement("div");
-  bookCard.classList.add("bookCard");
-
-  const cardTitle = document.createElement("h4");
-  const cardAuthor = document.createElement("p");
-  const cardPages = document.createElement("p");
-  const cardStatus = document.createElement("p");
-
-  const cardRemove = document.createElement("button");
-  cardRemove.textContent = "Remove";
-
-  cardTitle.textContent = book.title ? `Title: ${book.title}` : "Unknown Title";
-  cardAuthor.textContent = `Author: ${book.author}`;
-  cardPages.textContent = book.pages
-    ? `Pages: ${book.pages}`
-    : "Page count missing";
-  cardStatus.textContent = `Status: ${book.status}`;
-
-  bookCard.appendChild(cardTitle);
-  bookCard.appendChild(cardAuthor);
-  bookCard.appendChild(cardPages);
-  bookCard.appendChild(cardStatus);
-  bookCard.appendChild(cardRemove);
-
-  bookList.appendChild(bookCard);
-
-  cardRemove.addEventListener("click", () => removeBook(bookCard, book.uid));
-}
-
-function removeBook(bookElement, bookId) {
-  bookElement.remove();
-  myLibrary = myLibrary.filter((book) => book.uid !== bookId);
+  Book.addBook(newBook);
 }
 
 bookForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  addBookToLibrary();
+
+  handleBook();
+
   dialog.close();
   bookForm.reset();
 });
